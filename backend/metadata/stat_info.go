b7987ef9a301fd8ba7a5a3390eed5faf58bc3c86
goropikari <goropikari56@gmail.com>
2022-04-24 13:56:48 +0900

    implement stat info

---
+++ backend/metadata/stat_info.go
@@ -0,0 +1,32 @@
+package metadata
+
+import "github.com/goropikari/simpledbgo/backend/record"
+
+// StatInfo is a model of statistical infomation.
+type StatInfo struct {
+	numBlocks int
+	numRecs   int
+}
+
+// NewStatInfo construts a StatInfo.
+func NewStatInfo(numBlocks int, numRecs int) StatInfo {
+	return StatInfo{
+		numBlocks: numBlocks,
+		numRecs:   numRecs,
+	}
+}
+
+// EstNumBlocks returns estimated the number of blocks.
+func (info StatInfo) EstNumBlocks() int {
+	return info.numBlocks
+}
+
+// EstNumRecord returns estimated the number of records.
+func (info StatInfo) EstNumRecord() int {
+	return info.numRecs
+}
+
+// EstDistinctVals returns estimated the number of distinct values.
+func (info StatInfo) EstDistinctVals(fldname record.FieldName) int {
+	return 1 + (info.numRecs / 3)
+}
